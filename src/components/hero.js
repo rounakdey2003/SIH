3457dcfbc982e16a20c34687ae589fb1f33e2f20
rounakdey2003 <rounakriju2003@gmail.com>
2024-08-29 23:58:17 +0530

    upload3

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,12 +1,12 @@
 import Carousel from 'react-bootstrap/Carousel';
-
-import img1 from '../assets/images/img-hero1.png';
-import img2 from '../assets/images/img-hero2.png'
-import img3 from '../assets/images/img-hero3.png'
-import img4 from '../assets/images/img-hero4.png'
-import img5 from '../assets/images/img-hero5.png'
-import img6 from '../assets/images/img-hero6.png'
 import Container from "react-bootstrap/Container";
+
+import img1 from '../assets/images/banner1.png';
+import img2 from '../assets/images/banner2.png'
+import img3 from '../assets/images/banner3.png'
+import img4 from '../assets/images/banner4.png'
+import img5 from '../assets/images/banner5.png'
+
 
 function AppHero() {
     return (
@@ -60,15 +60,6 @@ function AppHero() {
                     <img
                         className="d-block w-100"
                         src={img5}
-                        alt="First slide"
-                    />
-                    <Carousel.Caption>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={img6}
                         alt="First slide"
                     />
                     <Carousel.Caption>
